ac908ed152d4faf8ad758447970c9b7a5a06bf4d
Yuri Nunes <yurimn14@outlook.com>
2023-07-02 19:22:28 -0300

    Aula 19 - Salvando e recuperando clientes

--- src/main/java/io/github/yuriney/VendasApplication.java
+++ src/main/java/io/github/yuriney/VendasApplication.java
@@ -1,11 +1,28 @@
 package io.github.yuriney;
 
+import io.github.yuriney.domain.entity.Cliente;
+import io.github.yuriney.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Dougllas"));
+            clientes.salvar(new Cliente("outro cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
     public static void main(String[] args) {
 
         SpringApplication.run(VendasApplication.class, args);
---
+++ src/main/java/io/github/yuriney/domain/repositorio/Clientes.java
@@ -0,0 +1,37 @@
+package io.github.yuriney.domain.repositorio;
+
+import io.github.yuriney.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class Clientes {
+
+    private static final String INSERT = "INSERT INTO cliente (nome) VALUES (?)";
+    private static final String SELECT_ALL = "SELECT nome FROM CLIENTE";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Cliente salvar(Cliente cliente) {
+        jdbcTemplate.update(INSERT, cliente.getNome());
+        return cliente;
+    }
+
+    public List<Cliente> obterTodos() {
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente(id, nome);
+            }
+        });
+    }
+}
